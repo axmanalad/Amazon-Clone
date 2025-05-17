3e24a191ca247bcc3083099aa0c99072b6b22813
axmanalad <alex.manalad8714@gmail.com>
2025-05-16 22:17:16 -0700

    Completed Lesson 1: JavaScript Basics

---
+++ intro-to-js/lesson-01/1a-j.js
@@ -0,0 +1,25 @@
+// Lesson 1 Exercises - JavaScript Basics
+// NOTE: Type 'allow pasting' in the Console to allow pasting code in the Console.
+
+// a. Use alert(...); to display 'Good morning!' in a popup.
+alert('Good morning!');
+// b. Display your name in a popup.
+alert('Alexander M.');
+// c. Using math, calculate 10 + 5 in the Console.
+console.log(10 + 5);
+// d. Calculate 20 -5 in the Console.
+console.log(20 - 5);
+// e. Calculate 2 + 2 - 5 in the Console.
+console.log(2 + 2 - 5);
+// f. Use document.body.innerHTML = ...; to display 'Good morning!' on the web page.
+document.body.innerHTML = 'Good morning!';
+//g Display your name on the web page.
+document.body.innerHTML = 'Alexander M.';
+
+// Challenges
+// h. You order a T-shirt for $10, socks for $8, and dinner plates for $20. Use JavaScript to calculate the total cost of your order.
+console.log(10 + 8 + 20);
+// i. Your bank account has $100, you spend $20 on lunch, $50 on dinner, and earn $200 from your job. Calculate how much money you have.
+console.log(100 - 20 - 50 + 200);
+// j, Use document.body.innerHTML = ...; to make the web page blank.
+document.body.innerHTML = '';
