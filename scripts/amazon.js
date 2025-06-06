5f526fd3ce6f0315f9de018ac0dcc54825012f43
axmanalad <alex.manalad8714@gmail.com>
2025-06-05 20:17:57 -0700

    Completed Lesson 13: Start the Amazon Project

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -56,6 +56,8 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+
+let addedToCartTimeoutId;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const { productId }  = button.dataset;
@@ -86,5 +88,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    let addedToCart = document.querySelector(`.js-added-to-cart-${productId}`);
+    addedToCart.classList.add('added-to-cart-visible');
+    clearTimeout(addedToCartTimeoutId);
+    addedToCartTimeoutId = setTimeout(function() {
+      addedToCart.classList.remove('added-to-cart-visible');
+    }, 2000);
   });
 });
