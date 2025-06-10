c7e22df3f48bd0fbbd6993059e4d373c39b4a5d6
axmanalad <alex.manalad8714@gmail.com>
2025-06-10 03:10:57 -0700

    Load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import {cart} from "../../data/cart-class.js";
+import { loadProducts } from "../../data/products.js";
 
 // Integration test for rendering the order summary
 describe('test suite: renderOrderSummary', () => {
@@ -7,6 +8,12 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
   const originalLocalStorage = window.localStorage;
   // Hooks
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
+
   beforeEach(() => {
     document.querySelector('.js-test-container').innerHTML = `
       <div class="js-order-summary"></div>
